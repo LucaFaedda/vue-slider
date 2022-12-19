71652b3dc90113c1bd34e62fa927b44d472e43a1
LucaFaedda <lucakarl91@gmail.com>
2022-12-19 14:14:15 +0100

    copiato file da es precedente

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// creato array di oggetti 
+
+const videogiochi = [
+    {
+        img: "01.webp",
+        titolo: "Spiderman: Miles Morales",
+        descrizione: "Il supereroe più amato di New York torna in una nuova veste"
+    },
+    {
+        img: "02.webp",
+        titolo: "Ratchet & Clank: Rift Apart",
+        descrizione: "L'ultima avvincente avventura di Ratchet e Clank, questa volta saranno affiancati da dei nuovi alleati"
+    },
+    {
+        img: "03.webp",
+        titolo: "Fortnite",
+        descrizione: "Beh, non c'è nemmeno bisogno di descrivere questo enorme gioco che ha portato al successo CiccioGamer"
+    },
+    {
+        img: "04.webp",
+        titolo: "Stray",
+        descrizione: "Vesti i panni di un simpatico gattino in un mondo post-apocalittico"
+    },
+    {
+        img: "05.webp",
+        titolo: "Marvel Avenger",
+        descrizione: "Tornano i vendicatori più forti del pianeta, una nuova minaccia imcombe"
+    }
+]
+
